a3c366cb270dffd8994da83d572248b3262cd032
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-02 16:15:40 -0300

    # commit - component folder - Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+class Home extends React.Component {        // Todo component React herda da classe React.Component e implementa o método Render.
+    render(){
+        
+    }
+}
